c90c0e3e40208bf1941097114834e318d1a9f073
Alex Espejo <aespejo102@gmail.com>
2026-03-03 16:33:49 -0800

    testing striped down deployment

--- server/server.js
+++ server/server.js
@@ -1,29 +1,10 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(
- cors({
-  origin: [
-   "http://localhost:5173",
-   "http://localhost:3000",
-   "https://ctc-vercel-testing-deploy-client.vercel.app/",
-  ],
- }),
-);
-app.use(express.json());
-
 app.get("/", (req, res) => {
- res.json({ message: "Hello from the server CTC test!" });
-});
-app.get("/api/health", (req, res) => {
- res.json({ status: "ok", message: "Server is running" });
-});
-
-app.get("/api/hello", (req, res) => {
- res.json({ message: "Hello from the server CTC test!" });
+ res.json({ message: "Hello World" });
 });
 
 if (process.env.NODE_ENV !== "production") {
